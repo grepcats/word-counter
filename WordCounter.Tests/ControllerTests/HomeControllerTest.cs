b28a6be31eed7234b5c4240fec85c98b8b4221d1
Kayla Ondracek <kayla.renee@gmail.com>
2018-02-16 10:06:36 -0800

    add test for Index View

---
+++ WordCounter.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+using WordCounter.Controllers;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index_ReturnIfView_True()
+        {
+            //arrange
+            HomeController controller = new HomeController();
+
+            //act
+            IActionResult indexView = controller.Index();
+            ViewResult result = indexView as ViewResult;
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+    }
+}
