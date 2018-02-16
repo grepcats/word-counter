e08262ffa79a0b89e43885544a15d4f1e2676082
Kayla Ondracek <kayla.renee@gmail.com>
2018-02-16 10:09:19 -0800

    add red test for correct model type for Index

--- WordCounter.Tests/ControllerTests/HomeControllerTest.cs
+++ WordCounter.Tests/ControllerTests/HomeControllerTest.cs
@@ -21,5 +21,18 @@ namespace WordCounter.Tests
             //Assert
             Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
+
+        [TestMethod]
+        public void Index_HasCorrectModelType_True()
+        {
+            //arrange
+            ViewResult indexView = new HomeController().Index() as ViewResult;
+
+            //act
+            var result = indexView.ViewData.Model;
+
+            //assert
+            Assert.IsInstanceOfType(result, typeof(RepeatCounter));
+        }
     }
 }
