0daee0360f640bb043585da727861a60a3f41705
Kayla Ondracek <kayla.renee@gmail.com>
2018-02-16 14:17:25 -0800

    add ability to submit up to 3 words to count

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using WordCounter.Models;
+using System;
 
 namespace WordCounter.Controllers
 {
@@ -16,13 +17,21 @@ namespace WordCounter.Controllers
         [HttpPost("/count")]
         public ActionResult Count()
         {
-            RepeatCounter newCounter = new RepeatCounter(Request.Form["word-input"], Request.Form["sentence-input"]);
-            if (newCounter.IsValid())
+            RepeatCounter.ClearAll();
+            RepeatCounter newCounter1 = new RepeatCounter(Request.Form["word-input1"], Request.Form["sentence-input"]);
+            RepeatCounter newCounter2 = new RepeatCounter(Request.Form["word-input2"], Request.Form["sentence-input"]);
+            RepeatCounter newCounter3 = new RepeatCounter(Request.Form["word-input3"], Request.Form["sentence-input"]);
+            List<RepeatCounter> allCounters = RepeatCounter.GetAll();
+            Console.WriteLine(allCounters.Count);
+            foreach (RepeatCounter counter in allCounters)
             {
-                newCounter.SetCount();
+                Console.WriteLine(counter.GetWord());
+                if (counter.IsValid())
+                {
+                    counter.SetCount();
+                }
             }
 
-            List<RepeatCounter> allCounters = RepeatCounter.GetAll();
             return View("Index", allCounters);
         }
 
